ef26b8f80559e0181e5b1a8a56f469a232806ad2
Julius A. Odai <jlsodai@gmail.com>
2025-05-24 14:14:48 +0200

    fix loading errors

--- src/components/LittlefootProvider.tsx
+++ src/components/LittlefootProvider.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { useEffect } from "react";
-import littlefoot from "littlefoot";
+import { usePathname } from "next/navigation";
 import "littlefoot/dist/littlefoot.css";
 
 export default function LittlefootProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   useEffect(() => {
-    littlefoot({
-      activateOnHover: true,
-      dismissOnUnhover: true,
-      buttonTemplate: `
-        <button
-            aria-label="Footnote <% number %>"
-            class="littlefoot__button"
-            id="<% reference %>"
-            title="See Footnote <% number %>"
-        />
-            <% number %>
-        </button>
-      `
+    let instance: any;
+    import("littlefoot").then((mod) => {
+      instance = mod.default({
+        activateOnHover: true,
+        dismissOnUnhover: true,
+        buttonTemplate: `
+          <button
+              aria-label="Footnote <% number %>"
+              class="littlefoot__button"
+              id="<% reference %>"
+              title="See Footnote <% number %>"
+          />
+              <% number %>
+          </button>
+        `
+      });
     });
-  }, []);
+
+    return () => {
+      if (instance && typeof instance.unmount === "function") {
+        instance.unmount();
+      }
+    };
+  }, [pathname]);
 
   return <>{children}</>;
 } 
