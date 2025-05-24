6ca15bc40173c702f2cd1f0d7cbcd75eaa32ef66
Julius A. Odai <jlsodai@gmail.com>
2025-05-24 17:25:13 +0200

    update home and logo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,18 +2,15 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
+    <main className="flex flex-1 flex-col justify-center text-center max-w-2xl mx-auto">
+      <img width="100" className="mx-auto" src="https://keepthefuturehuman.ai/wp-content/uploads/2025/03/KTFH-Logo-Dark.png" alt="Keep The Future Human Logo" />
+      <h2 className="text-2xl font-bold mt-8">We must not build AI to replace humans.</h2>
+      <p className="mt-4">Humanity is on the brink of developing artificial general intelligence that exceeds our own. It's time to close the gates on AGI and superintelligence... before we lose control of our future. Read  <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+          the essay
+        </Link></p>
     </main>
   );
 }
