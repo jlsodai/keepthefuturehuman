89ad5fc1a441abce6561d2ad9e7240cdde9f502f
Julius A. Odai <jlsodai@gmail.com>
2025-05-25 20:42:11 +0200

    add icon component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,19 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import * as lucideIcons from 'lucide-react';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  function Icon({ name, style, strokeWidth = 1.5, ...props }: { name: keyof typeof lucideIcons, style?: React.CSSProperties, strokeWidth?: number } & React.ComponentProps<'svg'>) {
+    const LucideIcon = lucideIcons[name];
+    if (!LucideIcon || typeof LucideIcon !== 'object' || !('render' in LucideIcon)) return null;
+    const Component = LucideIcon as unknown as React.ComponentType<any>;
+    return <Component style={{ display: 'inline-block', ...style }} strokeWidth={strokeWidth} {...props} />;
+  }
+
   return {
     ...defaultMdxComponents,
+    Icon,
     ...components,
   };
 }
