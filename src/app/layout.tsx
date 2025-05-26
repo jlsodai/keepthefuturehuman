d2872e4962dfeea32f1602c5eb3bf1e24f241df0
Julius A. Odai <jlsodai@gmail.com>
2025-05-26 05:46:07 +0200

    update link share image and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,35 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+export const metadata = {
+  title: 'Keep The Future Human',
+  description: "Humanity is on the brink of developing artificial general intelligence that exceeds our own. It's time to close the gates on AGI and superintelligence... before we lose control of our future.",
+  openGraph: {
+    title: 'Keep The Future Human',
+    description: "Humanity is on the brink of developing artificial general intelligence that exceeds our own. It's time to close the gates on AGI and superintelligence... before we lose control of our future.",
+    images: [
+      {
+        url: 'https://keepthefuturehuman.ai/wp-content/uploads/2025/03/KTFH-Homepage-Hero-Image.webp',
+        width: 1920, // Replace with actual width if different
+        height: 1080, // Replace with actual height if different
+        alt: 'KTFH Homepage Hero Image',
+        type: 'image/webp',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Keep The Future Human',
+    description: "Humanity is on the brink of developing artificial general intelligence that exceeds our own. It's time to close the gates on AGI and superintelligence... before we lose control of our future.",
+    images: [
+      {
+        url: 'https://keepthefuturehuman.ai/wp-content/uploads/2025/03/KTFH-Homepage-Hero-Image.webp',
+        alt: 'KTFH Homepage Hero Image',
+      },
+    ],
+  },
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
