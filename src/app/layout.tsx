fe3ed49645710cfb73d4ee8a00fc00f7e68175bd
Julius A. Odai <jlsodai@gmail.com>
2025-05-26 05:55:26 +0200

    disable google analytics on localhost

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,22 +38,27 @@ export const metadata = {
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-BDMRBJGSV5`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-BDMRBJGSV5');
-          `}
-        </Script>
+        {!isLocalhost && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=G-BDMRBJGSV5`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-BDMRBJGSV5');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
