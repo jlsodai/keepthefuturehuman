e29f0567cfe03e8118979c2cc20308e88e028354
Julius A. Odai <jlsodai@gmail.com>
2025-05-30 00:06:31 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,9 @@ export default function Layout({ children }: { children: ReactNode }) {
   const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
         {!isLocalhost && (
